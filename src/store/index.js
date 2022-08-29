30143417225478f7b1da0cee8f0551fb728fea3a
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-08-29 12:00:37 +0530

    applied a multiple state properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,31 @@
 import { legacy_createStore as createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "incease") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
-
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
